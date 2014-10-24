613c5b67aa5ff1de10cf1cac458b5fa6ff158ba7
vbas <valentinbas.dev@gmail.com>
2014-10-24 09:42:54 +0200

    start project

---
+++ app/src/main/java/com/example/raveh/rscontrol/SampleGattAttributes.java
@@ -0,0 +1,33 @@
+package com.example.raveh.rscontrol;
+
+/**
+ * Created by Raveh on 21/10/2014.
+ */
+
+import java.util.HashMap;
+
+/**
+ * This class includes a small subset of standard GATT attributes for demonstration purposes.
+ */
+public class SampleGattAttributes {
+    private static HashMap<String, String> attributes = new HashMap();
+    public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
+    public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+
+    static {
+        // Sample Services.
+        attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
+        attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
+        // Sample Characteristics.
+        attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
+        attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+
+        attributes.put("9a66fb0f-0800-9191-11e4-012d1540cb8e", "Battery level");
+        attributes.put("9a66fb0e-0800-9191-11e4-012d1540cb8e", "Information");
+    }
+
+    public static String lookup(String uuid, String defaultName) {
+        String name = attributes.get(uuid);
+        return name == null ? defaultName : name;
+    }
+}
